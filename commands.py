1adf0c03b8eab92aad9143b850c9013ed812a64b
Kornel Fulop <fkornel@gmail.com>
2025-06-01 00:23:51 +0200

    extract commands from plugin.py into commands.py

---
+++ commands.py
@@ -0,0 +1,10 @@
+import sublime_plugin
+
+class KornoteNewNoteCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		print("Kornote: new note command triggered")
+		self.window.status_message("Kornote: new note command triggered")
+
+		note_view = self.window.new_file()
+		note_view.set_name("Kornote Note")
+		note_view.set_scratch(True)
--- plugin.py
+++ plugin.py
@@ -1,19 +1,9 @@
 # pyright: reportMissingImports=false
 # pyright: reportUnusedImport=false
 
-import sublime
-import sublime_plugin
-import os
 from .db import init_db
 
 def plugin_loaded():
 	init_db()
 
-class KornoteNewNoteCommand(sublime_plugin.WindowCommand):
-	def run(self):
-		print("Kornote: new note command triggered")
-		self.window.status_message("Kornote: new note command triggered")
 
-		note_view = self.window.new_file()
-		note_view.set_name("Kornote Note")
-		note_view.set_scratch(True)
