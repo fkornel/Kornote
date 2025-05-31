cbf3a1461faf6bb9db0d0c8423c40bebf0a26c28
Kornel Fulop <fkornel@gmail.com>
2025-05-31 17:04:27 +0200

    Plugin skeleton

---
+++ plugin.py
@@ -0,0 +1,7 @@
+import sublime
+import sublime_plugin
+
+class KornoteNewNoteCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		print("Kornote: new note command triggered")
+		self.window.status_message("Kornote: new note command triggered")
