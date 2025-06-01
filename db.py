77ab56073099bbd9e2f00e7ee8e5e368d7c0a21c
Kornel Fulop <fkornel@gmail.com>
2025-06-01 11:41:38 +0200

    save or update a note in sqlite db

--- db.py
+++ db.py
@@ -1,11 +1,14 @@
+# pyright: reportMissingImports=false
+
 import sqlite3
 import os
 import sublime
+from datetime import datetime
 
 DB_PATH = os.path.join(sublime.packages_path(), "Kornote", "kornote.db")
 
 def init_db():
-	print("Kornote: initializing DB")
+	print("Kornote: db.init_db")
 	conn = sqlite3.connect(DB_PATH)
 	c = conn.cursor()
 	c.execute("""
@@ -19,4 +22,22 @@ def init_db():
 	""")
 	conn.commit()
 	conn.close()
-	print("Kornote: DB has been created")
+
+def save_note(note_id, title, content):
+	print("Kornote: db.save_note")
+	conn = sqlite3.connect(DB_PATH)
+	c = conn.cursor()
+
+	now = datetime.utcnow().isoformat()
+	c.execute("""
+		INSERT INTO notes (id, title, content, created_at, updated_at)
+		VALUES (?, ?, ?, ?, ?)
+		ON CONFLICT(id) DO UPDATE SET
+			title=excluded.title,
+			content=excluded.content,
+			updated_at=excluded.updated_at
+	""", (note_id, title, content, now, now))
+
+	conn.commit()
+	conn.close()
+
