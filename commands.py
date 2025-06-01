12a8601de87d07e0f84278bee3fe0d8d64782dd7
Kornel Fulop <fkornel@gmail.com>
2025-06-01 11:40:00 +0200

    generate note_id to a new note

--- commands.py
+++ commands.py
@@ -1,3 +1,4 @@
+import uuid
 import sublime_plugin
 
 class KornoteNewNoteCommand(sublime_plugin.WindowCommand):
@@ -5,6 +6,9 @@ class KornoteNewNoteCommand(sublime_plugin.WindowCommand):
 		print("Kornote: new note command triggered")
 		self.window.status_message("Kornote: new note command triggered")
 
-		note_view = self.window.new_file()
-		note_view.set_name("Kornote Note")
-		note_view.set_scratch(True)
+		note_id = str(uuid.uuid4())
+		view = self.window.new_file()
+		view.settings().set("kornote_id", note_id)
+		view.set_name("Untitled Note")
+		view.set_syntax_file("Packages/Text/Plain text.tmLanguage")
+		view.set_scratch(True)
